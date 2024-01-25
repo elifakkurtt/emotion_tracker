90016fa2e023f53bbe85aac74418da4eab39ba58
elifakkurtt <eelifakkurt@gmail.com>
2024-01-25 03:04:58 +0300

    jnknjkn

--- lib/controller/emotion_controller.dart
+++ lib/controller/emotion_controller.dart
@@ -1,4 +1,6 @@
+//import 'dart:convert';
 import 'package:get/get.dart';
+//import 'package:http/http.dart' as http;
 
 class EmotionController extends GetxController {
   RxList<Map<String, dynamic>> historyList = <Map<String, dynamic>>[].obs;
@@ -10,5 +12,17 @@ class EmotionController extends GetxController {
   List<Map<String, dynamic>> getHistoryList() {
     return historyList;
   }
- 
+
+  /*void getFavqs(String emotion) async {
+    try {
+      var response =
+          await http.get(Uri.parse('https://favqs.com/api/$emotion'));
+      if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      if(data==null){}
+      else();
+      }
+    } catch (e) {
+      print(e);
+    }*/
 }
